06195be52877116efc11f393e7f64d6c0d571d83
hcavalieri <hcavalieridoro@gmail.com>
2018-08-03 11:16:49 -0300

    Image fetching behavior and better documentation

--- README.md
+++ README.md
@@ -49,10 +49,11 @@ module.exports = {
 | projectId           | string  |                            | **[required]** Your Sanity project's ID                                                        |
 | dataset             | string  | production                 | The dataset to fetch from (can be tied to an .env file as needed)                              |
 | useCdn              | boolean | `true`                     | Whether to use Sanity's CDN or not. [Learn more](https://www.sanity.io/docs/api-cdn)           |
+| saveImages              | boolean | `false`                     | Whether to save images to disk. [This has limitations, though](#saving-images-to-your-filesystem).           |
 | queries             | array   |                            | **[required]** An array of objects that should contain the options below:                      |
-| (Query object) name | string  |                            | **[required]**  The name of the query. Used                                                    |
-| (Query object) type | string  | Defaults to the query name | Used to name the collection inside GraphQL                                                                                               |
+| (Query object) name | string  |                            | **[required]**  The name of the query, vital for the plugin's functioning                                                    |
 | (Query object) groq | string  |                            | **[required]** The actual [GROQ query](https://www.sanity.io/docs/data-store/how-queries-work). |
+| (Query object) type | string  | `'Sanity' + query.name` | Used to name the collection inside GraphQL                                                                                               |
 
 **PLEASE NOTE**: _All_ GROQ queries must contain the _id property as it'll be used as the internal ID for Gatsby's `createNode` function. Your build process will fail otherwise.
 
@@ -160,7 +161,7 @@ module.exports = {
 
 Sanity is a *API builder*, and for such, it's extremely hard to predict its data model: you can have all sorts of data types, with images nested 4 levels deep inside an object, for example. For such, **this plugin can't go far in shaping your nodes' format**.
 
-If you need extra fields built right into the nodes, or images saved to your file system with `gatsby-source-filesystem`'s `createRemoteFileNode`, then your option is to process them in your `gatsby-node.js` file through the [onCreateNode](https://next.gatsbyjs.org/docs/node-apis/#onCreateNode) API.
+If you need extra fields built right into the nodes, or deeply-nested images saved to your file system with `gatsby-source-filesystem`'s `createRemoteFileNode`, then your option is to process them in your `gatsby-node.js` file through the [onCreateNode](https://next.gatsbyjs.org/docs/node-apis/#onCreateNode) API.
 
 *Please note:* Gatsby's `createNodeField` action attaches your new field inside a `fields` object in your node, so when you query for your data in GraphQL, you'll have to do so by calling `fields { [FIELD-NAME] }`. I recommend that you play around with Graphiql to understand the data format before building your front-end.
 
@@ -190,38 +191,79 @@ exports.onCreateNode = ({ node, actions }) => {
 
 ### Saving images to your filesystem
 
+If you want to save images to the filesystem, you can use the `saveImages: true` option, but this will only work for those image objects nested in the root of fetched documents. If you need to save deeply nested images, you can either help me out figuring how to make it work with the plugin, or add them manually through your `gatsby-node.js` implementation.
+
 You'll have to know exactly where in you Sanity data tree your images are in order to save them to the filesystem. Here's a suggested implementation:
 
-(**TODO:** create an example for creating a remote file node in order to demonstrate how to save images to your repo. I'm still figuring which is the best way to implement this, so feel free to open an issue or do a PR and help me figure it out! :D)
+_Reminder_: You can also [serve images through Sanity](#serving-images-through-sanity).
 
-<!-- ```js
+```js
 // gatsby-node.js
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
+const imageUrlBuilder = require("@sanity/image-url");
 
-exports.onCreateNode = async ({ node, store, actions, cache }) => {
-  const { createNode, createNodeId } = actions;
+exports.onCreateNode = async ({ node, store, actions, cache, createNodeId }) => {
+  const { createNode, touchNode } = actions;
+
+  // Configuring the sanityClient
+  const Sanity = sanityClient({
+    projectId,
+    dataset,
+    useCdn,
+  });
   if (node.internal.type === 'Post') {
     // In this example, the image we want to save is the
-    // ogImage contained in the meta object
-    if (node.meta.ogImage) {
-      const fileNode = await createRemoteFileNode({
-        url: node.meta.ogImage,
-        store,
-        cache,
-        createNode,
-        createNodeId,
-      });
-      if (fileNode && fileNode.id) {
+    // ogImage contained in the meta object and considering
+    // we're fetching the whole image object, with _type and _asset
+    const { ogImage } = node.meta;
+    if (ogImage && ogImage.asset) {
+      // Build the url preserving hotspot 'n all
+      imageUrl = imageUrlBuilder(Sanity)
+        .image(ogImage.asset)
+        .url();
+
+      // Check if media is already downloaded
+      let fileNodeID;
+      const mediaDataCacheKey = `sanity-media-${imageUrl}`;
+      const cacheMediaData = await cache.get(mediaDataCacheKey);
+      if (cacheMediaData) {
+        fileNodeID = cacheMediaData.fileNodeID;
+        touchNode({ nodeId: cacheMediaData.fileNodeID });
+      }
+
+      // If not yet cached, createRemoteFileNode
+      if (!fileNodeID) {
+        try {
+          const fileNode = await createRemoteFileNode({
+            url: imageUrl,
+            store,
+            cache,
+            createNode,
+            createNodeId
+          });
+
+          if (fileNode) {
+            fileNodeID = fileNode.id;
+            await cache.set(mediaDataCacheKey, { fileNodeID });
+          }
+        } catch (error) {
+          console.error(`An image failed to be saved to internal storage: ${error}`)
+        }
+      }
+
+      // will be available at fields.ogImage and will
+      // work with gatsby-plugin-sharp out of the box
+      if (fileNodeID) {
         createNodeField({
           node,
-          name: 'image',
+          name: 'ogImage',
           value: fileNode.id,
         })
       } else { throw "Couldn't download the requested media file" }
     }
   }
 }
-``` -->
+```
 
 ## Serving images through Sanity
 
@@ -230,3 +272,17 @@ Alternatively, you can use [Sanity's image-url library](https://www.npmjs.com/pa
 Then you can LazyLoad these images and manipulate them to the client's device and connection through the `urlFor` function's parameter. More info on this, visit [the package's npm page](https://www.npmjs.com/package/@sanity/image-url).
 
 The downside of this approach is that you'd have to create lazy-loading components yourself (or use one from the community), and not have the magical blur / traced-SVG effect of the `gatsby-image` plugin.
+
+## TODO
+
+- Test for bugs in different environments and data structures (**I have not tested this with Gatsby v1 or v0, yet!**)
+- Better asset pipeline and file splitting (I've tried setting up Typescript and file bundling to no avail, if you can help out with this, it'd be awesome!)
+- Run the `normalizeNode` function deeper in the structure of the data to be able to save every single image to disk
+- Consider using ES6 classes to make client information universally available for all methods (not sure if gatsby-node would support this, though)
+- Gather feedback for new functionalities if needed
+
+## Credits
+
+First and foremost, a huge thanks to the good people backing Sanity for bringing such a joy to my developer life with this amazing CMS.
+
+As for the image-saving functionality, it was inspired heavily, at points copied, by [Nectum](https://github.com/nectum) at his [gatsby-source-test](https://github.com/nectum/GatsbyPluginTestBuilding/tree/master/plugins/gatsby-source-test) implementation.
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,23 +1,158 @@
+// Data normalizing heavily inspired by Nectum at:
+// https://github.com/nectum/GatsbyPluginTestBuilding/blob/master/plugins/gatsby-source-test/normalize.js
+// He, in turn, credits angeloashmore at:
+// https://github.com/angeloashmore/gatsby-source-prismic/blob/master/src/normalize.js
+
+
 const crypto = require('crypto');
 const sanityClient = require('@sanity/client');
+const imageUrlBuilder = require("@sanity/image-url");
+const { createRemoteFileNode } = require("gatsby-source-filesystem");
 
-exports.sourceNodes = async ({ actions }, configOptions) => {
-  const { createNode } = actions;
-  const { projectId, dataset = 'production', useCdn = true, queries } = configOptions;
+// export type Tqueries = {
+//   name?: string;
+//   groq?: string;
+//   type?: string;
+// }[];
+
+// export type Tobject = {
+//   [key: string]: any;
+// };
+
+// interface IconfigOptions {
+//   projectId?: string;
+//   dataset?: string;
+//   useCdn?: boolean;
+//   saveImages?: boolean;
+//   queries?: Tqueries;
+// }
+
+const colorizeLog = str => `\x1b[36m${str}\x1b[0m`;
+
+const capitalizeFirstLetter = str => `${str.substr(0,1).toUpperCase()}${str.substr(1,str.length).toLowerCase()}`
+
+const formatQuery = query => {
+  if (!query.name) {
+    throw "One or more queries are lacking a name.";
+  } else if (!query.groq) {
+    throw "One or more queries are missing a query! How would we even think about fetching data, huh?";
+  }
+  const groq = query.groq.replace(/\s/g, '');
+  return `"${query.name}": ${
+    groq.substr(groq.length - 1, 1) === ","
+      ? groq.slice(0, groq.length - 1)
+      : groq
+    }`;
+}
+
+const fetchData = async (queries, client) => {
+  // Creating a single query that will be ran by Sanity and return
+  // an object with keys equivalent to the queries' names
+  const finalQuery = `{
+    ${queries.map(formatQuery).join(',')}
+  }`.replace(/\s/g,"");
+
+  console.time(colorizeLog('\nSanity\'s data fetched in'));
+  const data = await client.fetch(finalQuery);
+  console.timeEnd(colorizeLog('\nSanity\'s data fetched in'));
+  return data;
+};
+
+// field: any
+const isImage = field => {
+  return field._type && field._type === 'image';
+}
+
+// node: Object<any>
+const normalizeNode = async (node, helpers) => {
+  const { urlFor, cache, store, createNode, createNodeId, touchNode } = helpers;
+  for (const key of Object.keys(node)) {
+    const field = node[key];
+
+    // We can't work on fields that aren't objects
+    // (arrays are also objects)
+    if (typeof field !== 'object') {
+      continue
+    } else if (isImage(field)) {
+      // if the field doesn't have the asset field,
+      // then we won't mess with it
+      if (!field.asset) {
+        continue
+      }
+
+      // Build the URL for the image using Sanity's package
+      const imageUrl = urlFor(field.asset).url();
+      if (imageUrl) {
+        let fileNodeID;
+        const mediaDataCacheKey = `sanity-media-${imageUrl}`;
+        const cacheMediaData = await cache.get(mediaDataCacheKey);
+
+        if (cacheMediaData) {
+          fileNodeID = cacheMediaData.fileNodeID;
+          touchNode({ nodeId: cacheMediaData.fileNodeID });
+        }
+
+        if (!fileNodeID) {
+          try {
+            const fileNode = await createRemoteFileNode({
+              url: imageUrl,
+              store,
+              cache,
+              createNode,
+              createNodeId
+            });
+
+            if (fileNode) {
+              fileNodeID = fileNode.id;
+              await cache.set(mediaDataCacheKey, { fileNodeID });
+            }
+          } catch (error) {
+            console.error(`An image failed to be saved to internal storage: ${error}`)
+          }
+        }
+
+        if (fileNodeID) {
+          // after stored in cache, add this file node id to
+          // a field localFile__NODE in the root of the
+          // image object and also add the imageUrl field
+          Object.assign(node, {
+            [key]: {
+              ...field,
+              imageUrl,
+              localFile__NODE: fileNodeID,
+            }
+          })
+        }
+      } else {
+        console.error(`An image field has an incomplete asset object or something went wrong when creating its URL: ${key}`);
+      }
+    } else {
+      // If it's an object with a _type !== 'image'
+      // then we want to go deeper in the structure and
+      // normalize it again... however, creating a circular
+      // structure is dangerous and simply can't happen...
+      // if you have an idea to fix this, please submit an issue ;)
+
+      // const newField = normalizeNode(field);
+      // if (newField) {
+      //   Object.assign(newField, {
+      //     [key]: newField
+      //   });
+      // }
+    }
+  }
+  return node;
+}
+
+exports.sourceNodes = async ({ actions, cache, store, createNodeId }, configOptions) => {
+  const { createNode, touchNode } = actions;
+  const { projectId, dataset = 'production', useCdn = true, saveImages = false, queries } = configOptions;
 
   if (!projectId) {
-    throw "No ID found for your Sanity project!"
-  } else if (!queries) {
-    throw "Please add the queries to be fetched from Sanity"
+    throw new Error ("No ID found for your Sanity project!");
+  } else if (!queries || !queries[0]) {
+    throw new Error ("Please add the queries to be fetched from Sanity");
   }
-
-  queries.forEach(query => {
-    if (!query.name) {
-      throw "One or more queries are missing a name"
-    } else if (!query.groq) {
-      throw "One or more queries are missing a query! How would we even think about fetching data, huh?"
-    }
-  });
 
   // Configuring the sanityClient
   const Sanity = sanityClient({
@@ -25,56 +160,72 @@ exports.sourceNodes = async ({ actions }, configOptions) => {
     dataset,
     useCdn,
   });
+  const builder = imageUrlBuilder(Sanity);
 
-  // Creating a single query that will be ran by Sanity and return
-  // an object with keys equivalent to the queries' names
-  const formattedQueries = queries.map(query => {
-    const groq = query.groq.replace(/\s/g, '');
-    return `"${query.name}": ${
-      groq.substr(groq.length -1, 1) === ',' ?
-        groq.slice(0, groq.length - 1)
-      : groq
-    }`
-  });
-  const finalQuery = `{
-    ${formattedQueries.join(',')}
-  }`
-  const data = await Sanity.fetch(finalQuery);
+  const urlFor = asset => builder.image(asset);
+
+  // Fetching data through the ./fetchData.ts module
+  const data = await fetchData(queries, Sanity);
 
   if (data) {
     // For each query listed in the config, loop through
     // the array inside the data object that contains a key
     // equivalent to the query name and create a node from there
-    queries.forEach(query => {
-      if (data[query.name]) {
-        data[query.name].forEach(node => {
-
-          if (!node._id) {
-            throw "Every query should fetch the _id property to be used internally by Gatsby"
+    if (saveImages) {
+      console.time(colorizeLog('\nSanity images saved to disk in'));
+    }
+    for (const query of queries) {
+      if (query.name && data[query.name]) {
+        for (const node of data[query.name]) {
+          // Provide helpful error in case the user
+          // fetches a non-object node as these can't
+          // be translated into nodes for Gatsby
+          if (typeof node !== 'object') {
+            console.warn(`${
+              typeof node == 'string' ?
+                `${node.substr(0, 10)}...`
+                : 'A node'
+              } is not an object. It will not be parsed into a new node`);
+            continue
           }
 
-          const type = query.type || query.name;
+          if (!node._id) {
+            throw new Error("Every query should fetch the _id property to be used internally by Gatsby")
+          }
+
+          let finalNode = node;
+          if (saveImages) {
+            finalNode = await normalizeNode(
+              node,
+              { urlFor, cache, store, touchNode, createNodeId, createNode }
+            );
+          }
+
+          const type = query.type || `Sanity${capitalizeFirstLetter(query.name)}`;
           createNode({
-            ...node,
+            ...finalNode,
 
             // mandatory fields
-            id: node._id,
+            id: finalNode._id,
             parent: null,
             children: [],
             internal: {
               type,
               contentDigest: crypto
                 .createHash(`md5`)
-                .update(JSON.stringify(node))
+                .update(JSON.stringify(finalNode))
                 .digest(`hex`),
-              description: `${type}-${node._id}`,
+              description: `${type}-${finalNode._id}`,
             }
           })
-        })
+        }
       } else {
-        console.log(`${name} doesn't exist in incoming data`);
+        console.error(`${query.name} doesn't exist in incoming data`);
       }
-    })
+    }
+    if (saveImages) {
+      console.timeEnd(colorizeLog('\nSanity images saved to disk in'));
+    }
   } else {
     throw "Failed to fetch data :("
   }
