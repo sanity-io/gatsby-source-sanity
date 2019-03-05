b8086967427ffdc1bf0ee92ff9df1b0806bd1af0
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-03-05 16:19:07 +0100

    Remove unused dependencies

--- src/types/easyGraphQL.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'easygraphql-mock' {
-  type MockMap = {[key: string]: {[key: string]: any}}
-  type ScalarMockMap = {[key: string]: any}
-
-  function mockSchema(sdl: string, providedValues?: MockMap): MockMap
-  function mockSchema(sdl: string, providedValues?: ScalarMockMap): MockMap
-
-  export = mockSchema
-}
