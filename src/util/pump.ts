ea9e51170615b50841c77048c6279f6ca8474546
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-03-10 12:46:24 -0800

    fix(pump): correct return value of pump utility

--- src/util/pump.ts
+++ src/util/pump.ts
@@ -2,7 +2,7 @@ import pumpIt from 'pump'
 
 type Stream = NodeJS.ReadableStream | NodeJS.WritableStream
 
-export function pump(streams: Stream[]): Promise<undefined> {
+export function pump(streams: Stream[]): Promise<void> {
   return new Promise((resolve, reject) =>
     pumpIt(streams, (err) => {
       if (err) {
