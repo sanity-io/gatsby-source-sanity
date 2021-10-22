8013a189c059a8c8d380b5a667a4cecae73b004f
Henrique Doro <opensource@hdoro.dev>
2021-10-22 08:46:13 -0300

    refactor(webhooks): expect body only for delete ops

---
+++ src/util/handleDeltaChanges.ts
@@ -0,0 +1,70 @@
+import {SanityClient} from '@sanity/client'
+import {SourceNodesArgs} from 'gatsby'
+import {getTypeName, ProcessingOptions, toGatsbyNode} from './normalize'
+import debug from '../debug'
+import {SanityDocument} from '../types/sanity'
+import { registerBuildTime } from './getPluginStatus'
+
+export const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+// Ensures document changes are persisted to the query engine.
+const SLEEP_DURATION = 500
+
+/**
+ * Queries all documents changed since last build & adds them to Gatsby's store
+ */
+export default async function handleDeltaChanges({
+  args,
+  lastBuildTime,
+  client,
+  processingOptions,
+}: {
+  args: SourceNodesArgs
+  lastBuildTime: Date
+  client: SanityClient
+  processingOptions: ProcessingOptions
+}): Promise<boolean> {
+  await sleep(SLEEP_DURATION)
+
+  try {
+    const changedDocs = await client.fetch(
+      '*[!(_type match "system.**") && _updatedAt > $timestamp]',
+      {
+        timestamp: lastBuildTime.toISOString(),
+      },
+    )
+    console.log({changedDocs})
+    handleChangedDocuments(args, changedDocs, processingOptions)
+    registerBuildTime(args)
+    args.reporter.info(`[sanity] ${changedDocs.length} documents updated.`)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+function handleChangedDocuments(
+  args: SourceNodesArgs,
+  changedDocs: SanityDocument[],
+  processingOptions: ProcessingOptions,
+) {
+  const {reporter} = args
+  const {typeMap} = processingOptions
+
+  return changedDocs.reduce((count, doc) => {
+    const type = getTypeName(doc._type)
+    if (!typeMap.objects[type]) {
+      reporter.warn(
+        `[sanity] Document "${doc._id}" has type ${doc._type} (${type}), which is not declared in the GraphQL schema. Make sure you run "graphql deploy". Skipping document.`,
+      )
+      return count
+    }
+
+    debug('%s document with ID %s', 'Changed', doc._id)
+    processingOptions.createNode(toGatsbyNode(doc, processingOptions))
+    return count + 1
+  }, 0)
+}
+
