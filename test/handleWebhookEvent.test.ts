d07310ce586c616320454635a20a55942f2e40c9
Henrique Doro <opensource@hdoro.dev>
2021-11-15 09:01:11 -0300

    tests: update webhook test to match new approach

--- test/handleWebhookEvent.test.ts
+++ test/handleWebhookEvent.test.ts
@@ -1,38 +1,19 @@
-import {SanityWebhookV1Body, SanityWebhookV2Body} from '../src/types/sanity'
+import {SanityWebhookDeleteBody} from '../src/types/sanity'
 import {validateWebhookPayload} from '../src/util/handleWebhookEvent'
 
-const V2: SanityWebhookV2Body = {
+const DeleteHook: SanityWebhookDeleteBody = {
   dataset: 'production',
   projectId: 'projectId',
-  operation: 'create',
-  __webhooksVersion: 'v2',
   documentId: 'doc',
-  after: {
-    _id: '_id',
-    _rev: '_rev',
-    _createdAt: '_createdAt',
-    _updatedAt: '_updatedAt',
-    _type: '_type',
-  },
+  operation: 'delete',
 }
 
-const V2Faulty: SanityWebhookV2Body = {
+const DeleteHookFaulty = {
   dataset: 'production',
   projectId: 'projectId',
-  operation: 'create',
-  // Different version
-  __webhooksVersion: '2' as any,
-  documentId: 'doc',
-  after: {
-    _id: '_id',
-    _rev: '_rev',
-    _createdAt: '_createdAt',
-    _updatedAt: '_updatedAt',
-    _type: '_type',
-  },
 }
 
-const V1: SanityWebhookV1Body = {
+const V1Hooks = {
   ids: {
     created: [],
     updated: [],
@@ -40,49 +21,18 @@ const V1: SanityWebhookV1Body = {
   },
 }
 
-const V1Faulty = {
-  ids: {
-    updated: [],
-    deleted: [],
-  },
-}
-
-const V2withIds = {
-  dataset: 'production',
-  projectId: 'projectId',
-  operation: 'create',
-  __webhooksVersion: 'v2',
-  documentId: 'doc',
-  ids: {
-    created: [],
-    updated: [],
-    deleted: [],
-  },
-  after: {
-    _id: '_id',
-    _rev: '_rev',
-    _createdAt: '_createdAt',
-    _updatedAt: '_updatedAt',
-    _type: '_type',
-  },
-}
-
-test('webhook payload - V2', () => {
-  expect(validateWebhookPayload(V2)).toEqual('v2')
+test('webhook payload - delete', () => {
+  expect(validateWebhookPayload(DeleteHook)).toEqual('delete-operation')
 })
 
-test('webhook payload - faulty V2', () => {
-  expect(validateWebhookPayload(V2Faulty)).toEqual(false)
+test('webhook payload - delete faulty', () => {
+  expect(validateWebhookPayload(DeleteHookFaulty)).toEqual(false)
 })
 
 test('webhook payload - V1', () => {
-  expect(validateWebhookPayload(V1)).toEqual('v1')
+  expect(validateWebhookPayload(V1Hooks)).toEqual(false)
 })
 
-test('webhook payload - faulty V1', () => {
-  expect(validateWebhookPayload(V1Faulty as any)).toEqual(false)
-})
-
-test('webhook payload - V2 w/ ids', () => {
-  expect(validateWebhookPayload(V2withIds as any)).toEqual('v2')
+test('webhook payload - create/update', () => {
+  expect(validateWebhookPayload({})).toEqual(false)
 })
