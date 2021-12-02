dea372ea12737d127c1b94f658cf5c2503943899
kathmbeck <katherine.murphy.beck@gmail.com>
2021-12-02 11:37:50 -0800

    node manifestt api 2.0 updates

--- src/util/createNodeManifest.ts
+++ src/util/createNodeManifest.ts
@@ -1,9 +1,16 @@
 import {Actions, Node, SourceNodesArgs} from 'gatsby'
+import { getGatsbyVersion } from 'gatsby-core-utils'
+import { lt, prerelease } from 'semver' 
 import debug from '../debug'
 import {SanityInputNode} from '../types/gatsby'
 
-const ONE_WEEK = 1000 * 60 * 60 * 24 * 7 // ms * sec * min * hr * day
-let nodeManifestWarningWasLogged: boolean
+let warnOnceForNoSupport: boolean
+let warnOnceToUpgradeGatsby: boolean
+
+const GATSBY_VERSION_MANIFEST_V2 = `4.3.0`
+const gatsbyVersion = getGatsbyVersion()
+const gatsbyVersionIsPrerelease = prerelease(gatsbyVersion)
+const shouldUpgradeGatsbyVersion = lt(gatsbyVersion, GATSBY_VERSION_MANIFEST_V2) && !gatsbyVersionIsPrerelease
 
 export default function createNodeManifest(
   actions: Actions,
@@ -13,7 +20,7 @@ export default function createNodeManifest(
 ) {
   try {
     const {unstable_createNodeManifest} = actions as Actions & {
-      unstable_createNodeManifest: (props: {manifestId: string; node: Node}) => void
+      unstable_createNodeManifest: (props: {manifestId: string; node: Node; updatedAtUTC: string}) => void
     }
     const {getNode} = args
     const type = node.internal.type
@@ -24,22 +31,24 @@ export default function createNodeManifest(
     const shouldCreateNodeManifest = createNodeManifestIsSupported && nodeTypeNeedsManifest
 
     if (shouldCreateNodeManifest) {
+      if (shouldUpgradeGatsbyVersion && !warnOnceToUpgradeGatsby) {
+        console.warn(
+          `Your site is doing more work than it needs to for Preview, upgrade to Gatsby ^${GATSBY_VERSION_MANIFEST_V2} for better performance`
+        )
+        warnOnceToUpgradeGatsby = true
+      }
+
       const updatedAt = new Date((node._updatedAt as string) || Date.now())
-      const nodeWasRecentlyUpdated =
-        Date.now() - updatedAt.getTime() <=
-        // Default to only create manifests for items updated in last week
-        (process.env.CONTENT_SYNC_SANITY_HOURS_SINCE_ENTRY_UPDATE || ONE_WEEK)
-      if (!nodeWasRecentlyUpdated) return
 
       const nodeForManifest = getNode(node.id) as Node
       const manifestId = `${publishedId}-${updatedAt.toISOString()}`
 
-      unstable_createNodeManifest({manifestId, node: nodeForManifest})
-    } else if (!createNodeManifestIsSupported && !nodeManifestWarningWasLogged) {
+      unstable_createNodeManifest({manifestId, node: nodeForManifest, updatedAtUTC: updatedAt.toUTCString()})
+    } else if (!createNodeManifestIsSupported && !warnOnceForNoSupport {
       args.reporter.warn(
         `Sanity: Your version of Gatsby core doesn't support Content Sync (via the unstable_createNodeManifest action). Please upgrade to the latest version to use Content Sync in your site.`,
       )
-      nodeManifestWarningWasLogged = true
+      warnOnceForNoSupport = true
     }
   } catch (e) {
     let result = (e as Error).message
