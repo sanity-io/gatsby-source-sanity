fa9a40c77497415a36bfca4908e2e80ba8dfd25c
kathmbeck <katherine.murphy.beck@gmail.com>
2021-12-09 08:49:48 -0800

    semver fix

--- src/util/createNodeManifest.ts
+++ src/util/createNodeManifest.ts
@@ -8,7 +8,7 @@ let warnOnceForNoSupport: boolean
 let warnOnceToUpgradeGatsby: boolean
 
 const GATSBY_VERSION_MANIFEST_V2 = `4.3.0`
-const gatsbyVersion = getGatsbyVersion()
+const gatsbyVersion = (typeof getGatsbyVersion === `function` && getGatsbyVersion()) || `0.0.0`
 const gatsbyVersionIsPrerelease = prerelease(gatsbyVersion)
 const shouldUpgradeGatsbyVersion =
   lt(gatsbyVersion, GATSBY_VERSION_MANIFEST_V2) && !gatsbyVersionIsPrerelease
