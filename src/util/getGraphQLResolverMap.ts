aa359babff2530964198b1dbeaa17f15f97081ad
Matt Kane <m@mk.gg>
2023-07-26 11:44:03 +0100

    fix: use resolver for mixed document/node unions

--- src/util/getGraphQLResolverMap.ts
+++ src/util/getGraphQLResolverMap.ts
@@ -1,10 +1,26 @@
 import {camelCase} from 'lodash'
 import {CreateResolversArgs} from 'gatsby'
 import {GraphQLFieldResolver} from 'gatsby/graphql'
-import {GatsbyResolverMap, GatsbyGraphQLContext} from '../types/gatsby'
+import {GatsbyResolverMap, GatsbyGraphQLContext, GatsbyNodeModel} from '../types/gatsby'
 import {TypeMap, FieldDef} from './remoteGraphQLSchema'
 import {resolveReferences} from './resolveReferences'
 import {PluginConfig} from './validateConfig'
+import {getConflictFreeFieldName, getTypeName} from './normalize'
+import {SanityRef} from '../types/sanity'
+
+/**
+ * Is the type a union with both document and non-document types
+ */
+function isMixedUnion(typeName: string, typeMap: TypeMap) {
+  const union = typeMap.unions[typeName]
+  if (!union) {
+    return false
+  }
+  return (
+    union.types.some((typeName) => typeMap.objects[typeName]?.isDocument) &&
+    union.types.some((typeName) => !typeMap.objects[typeName]?.isDocument)
+  )
+}
 
 export function getGraphQLResolverMap(
   typeMap: TypeMap,
@@ -15,6 +31,21 @@ export function getGraphQLResolverMap(
   Object.keys(typeMap.objects).forEach((typeName) => {
     const objectType = typeMap.objects[typeName]
     const fieldNames = Object.keys(objectType.fields)
+
+    // Add resolvers for unions
+    resolvers[objectType.name] = fieldNames
+      .map((fieldName) => ({fieldName, ...objectType.fields[fieldName]}))
+      .filter((field) =>
+        isMixedUnion(getTypeName(field.namedType.name.value, pluginConfig.typePrefix), typeMap),
+      )
+      .reduce((fields, field) => {
+        const targetField = objectType.isDocument
+          ? getConflictFreeFieldName(field.fieldName, pluginConfig.typePrefix)
+          : field.fieldName
+
+        fields[targetField] = {resolve: getResolver(field, pluginConfig.typePrefix)}
+        return fields
+      }, resolvers[objectType.name] || {})
 
     // Add raw resolvers
     resolvers[objectType.name] = fieldNames
@@ -27,6 +58,39 @@ export function getGraphQLResolverMap(
   })
 
   return resolvers
+}
+
+function getResolver(
+  field: FieldDef & {fieldName: string},
+  typePrefix?: string,
+): GraphQLFieldResolver<{[key: string]: any}, GatsbyGraphQLContext> {
+  return (source, args, context) => {
+    if (field.isList) {
+      const items: SanityRef[] = source[field.fieldName] || []
+      return items && Array.isArray(items)
+        ? items.map((item) => maybeResolveReference(item, context.nodeModel, typePrefix))
+        : []
+    }
+
+    const item: SanityRef | undefined = source[field.fieldName]
+    return maybeResolveReference(item, context.nodeModel, typePrefix)
+  }
+}
+
+function maybeResolveReference(
+  item: {_ref?: string; _type?: string; internal?: {}} | undefined,
+  nodeModel: GatsbyNodeModel,
+  typePrefix?: string,
+) {
+  if (item && typeof item._ref === 'string') {
+    return nodeModel.getNodeById({id: item._ref})
+  }
+
+  if (item && typeof item._type === 'string' && !item.internal) {
+    return {...item, internal: {type: getTypeName(item._type, typePrefix)}}
+  }
+
+  return item
 }
 
 function getRawResolver(
