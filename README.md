f2e6e152634d0de65ffeeab229a1a5fb28414d36
Rune Botten <rbotten@gmail.com>
2023-08-25 11:10:15 -0700

    chore: add README symlink in root

---
+++ README.md
@@ -0,0 +1,1 @@
+packages/gatsby-source-sanity/README.md
--- examples/blog/gatsby-config.js
+++ examples/blog/gatsby-config.js
@@ -12,6 +12,16 @@ module.exports = {
         dataset: 'production',
       },
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        // https://77i5ch25.api.sanity.io/v1/graphql/production/default
+        projectId: '77i5ch25',
+        dataset: 'production',
+        token: 'skQKEn6900875oGYrSq2yrHfb87BYoYKiJiqj9FvNrPIbenbSewqGxGCDEfZ00eJul3sKsjVUhVqExxJOl8TwzSFnv29zbfFWBaG8qkWmKB60vpFvP7eobodxBEwHsK2ONub6i5VfNUUNJgg9w5GQZNBz0IPtGIXlIIqKLZmkEX1Lt5sB8P6',
+        //typePrefix: 'runeb',
+      },
+    },
     `gatsby-plugin-image`,
   ],
 }
