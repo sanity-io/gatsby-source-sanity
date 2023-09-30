1a623fd46e2a96dfa9b4d9be7b96091abb854557
Rune Botten <rbotten@gmail.com>
2023-09-29 17:25:58 -0700

    feat: make tests pass again by introducing Union types for CDRs with multiple target types

--- packages/gatsby-source-sanity/src/util/mapCrossDatasetReferences.ts
+++ packages/gatsby-source-sanity/src/util/mapCrossDatasetReferences.ts
@@ -1,15 +1,28 @@
-import {parse, visit, print, Kind, type DirectiveNode, type NamedTypeNode} from 'graphql'
+import {
+  parse,
+  visit,
+  print,
+  Kind,
+  type DirectiveNode,
+  type NamedTypeNode,
+  type UnionTypeDefinitionNode,
+} from 'graphql'
 
-// Function to extract value from a ValueNode
-const getStringArgumentValueFromDirective = (directive: DirectiveNode, argument: string) => {
+// Function to extract array of string value from a ValueNode
+const getStringArrayArgumentValuesFromDirective = (directive: DirectiveNode, argument: string) => {
+  const values: string[] = []
   for (const arg of directive.arguments || []) {
     if (arg.name.value === argument) {
-      if (arg.value.kind === Kind.STRING) {
-        return arg.value.value
+      if (arg.value.kind === Kind.LIST) {
+        arg.value.values.forEach((value) => {
+          if (value.kind === Kind.STRING) {
+            values.push(value.value)
+          }
+        })
       }
     }
   }
-  return
+  return values
 }
 
 const referenceDirectiveNode: DirectiveNode = {
@@ -20,6 +33,10 @@ const referenceDirectiveNode: DirectiveNode = {
   },
 }
 
+const typeNameForMappping = (typeNames: string[]) => {
+  return typeNames.join('Or')
+}
+
 // This function rewrites the Schema to treat crossDatasetReference fields as
 // references, since we will assume the required schemas and content is added as
 // additional source plugin configurations.
@@ -27,7 +44,7 @@ export function mapCrossDatasetReferences(api: string) {
   const astNode = parse(api)
 
   // First pass: Find any named types that have cdr directives on them
-  const cdrMapping: Record<string, string> = {}
+  const cdrMapping: Record<string, string[]> = {}
   visit(astNode, {
     enter(node) {},
     [Kind.OBJECT_TYPE_DEFINITION](node) {
@@ -35,33 +52,64 @@ export function mapCrossDatasetReferences(api: string) {
         const cdrDirective = node.directives?.find((d) => d.name.value === 'cdr')
         if (cdrDirective) {
           // This is a type that has a cdr directive on it
-          let typeName = getStringArgumentValueFromDirective(cdrDirective, 'typeName')
-          if (typeName) {
-            cdrMapping[node.name.value] = typeName
+          //let typeName = getStringArgumentValueFromDirective(cdrDirective, 'typeName')
+          let typeNames = getStringArrayArgumentValuesFromDirective(cdrDirective, 'typeNames')
+          if (typeNames) {
+            cdrMapping[node.name.value] = typeNames
           }
         }
       }
     },
   })
 
+  const unionDefinitions: UnionTypeDefinitionNode[] = []
+  // Find all the values in cdrMapping that have more than 1 value
+  for (const key in cdrMapping) {
+    if (cdrMapping[key].length > 1) {
+      // Create a union type for this set of types
+      unionDefinitions.push({
+        kind: Kind.UNION_TYPE_DEFINITION,
+        name: {
+          kind: Kind.NAME,
+          value: typeNameForMappping(cdrMapping[key]),
+        },
+        types: cdrMapping[key].map((key) => {
+          return {
+            kind: Kind.NAMED_TYPE,
+            name: {
+              kind: Kind.NAME,
+              value: key,
+            },
+          }
+        }),
+      })
+    }
+  }
+
+  // Add our new Union types
+  const newAST = {
+    ...astNode,
+    definitions: [...astNode.definitions, ...unionDefinitions]
+  }
+
   // Second pass: Rewrite the schema to replace CDR types with the appropriate
   // target type and add @reference directive where appropriate
-  const modifiedTypes = visit(astNode, {
+  const modifiedTypes = visit(newAST, {
     [Kind.FIELD_DEFINITION](fieldNode) {
       const cdrDirective = fieldNode.directives?.find((d) => d.name.value === 'cdr')
-      let mappedTypeName: string | undefined
+      let mappedTypeNames: string[] | undefined = undefined
 
       if (fieldNode.type.kind === Kind.NAMED_TYPE) {
         if (cdrDirective) {
           // This is a field that has a cdr directive on it
           // TODO: look up any configured typePrefixes for the dataset
-          mappedTypeName = getStringArgumentValueFromDirective(cdrDirective, 'typeName')
+          mappedTypeNames = getStringArrayArgumentValuesFromDirective(cdrDirective, 'typeNames')
         } else {
           // This is a field that does not have a cdr directive on it
           // Check if the field type is a CDR type
           const fieldName = fieldNode.type.name.value
           if (fieldName in cdrMapping) {
-            mappedTypeName = cdrMapping[fieldName]
+            mappedTypeNames = cdrMapping[fieldName]
           }
         }
       }
@@ -71,11 +119,11 @@ export function mapCrossDatasetReferences(api: string) {
         const fieldName = innerType.name.value
         // Check if the field type is a CDR type
         if (fieldName in cdrMapping) {
-          mappedTypeName = cdrMapping[fieldName]
+          mappedTypeNames = cdrMapping[fieldName]
         }
       }
 
-      if (mappedTypeName) {
+      if (mappedTypeNames) {
         // Replace the cdr directive with a reference directive and replace
         // the type name with the actual target type
         let directives: DirectiveNode[] = (fieldNode.directives || []).filter(
@@ -94,7 +142,7 @@ export function mapCrossDatasetReferences(api: string) {
               ...fieldNode.type,
               name: {
                 ...fieldNode.type.name,
-                value: mappedTypeName,
+                value: typeNameForMappping(mappedTypeNames),
               },
             },
           }
@@ -109,7 +157,7 @@ export function mapCrossDatasetReferences(api: string) {
                 kind: Kind.NAMED_TYPE,
                 name: {
                   kind: Kind.NAME,
-                  value: mappedTypeName,
+                  value: typeNameForMappping(mappedTypeNames),
                 },
               },
             },
@@ -120,26 +168,26 @@ export function mapCrossDatasetReferences(api: string) {
     },
     [Kind.UNION_TYPE_DEFINITION](node) {
       // Check if any of the union types match keys in the cdrMapping
-      const modifiedTypes = node.types?.map(typeNode => {
-        const typeName = typeNode.name.value;
+      const modifiedTypes = node.types?.map((typeNode) => {
+        const typeName = typeNode.name.value
         if (cdrMapping[typeName]) {
           return {
             ...typeNode,
             name: {
               kind: Kind.NAME,
-              value: cdrMapping[typeName]
-            }
-          };
+              value: cdrMapping[typeName],
+            },
+          }
         }
-        return typeNode;  // return the type unchanged if it's not in the cdrMapping
-      });
-  
+        return typeNode // return the type unchanged if it's not in the cdrMapping
+      })
+
       // Return the modified union node
       return {
         ...node,
-        types: modifiedTypes
-      };
-    }
+        types: modifiedTypes,
+      }
+    },
   })
 
   return print(modifiedTypes)
